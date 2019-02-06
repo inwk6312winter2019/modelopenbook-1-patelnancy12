2b956f787d864979ad1564bdf0267b090e51b7c1
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-06 16:25:30 +0000

    task initiated

--- model1.py
+++ model1.py
@@ -1,1 +1,27 @@
 # ths is open book test model 1
+
+fopen=open("running-config.cfg",'r')
+fopen=fopen.read()
+fopen=fopen.split("\n")
+final_list = list()
+interface_list = list()
+intdict=dict()
+access_list =  list()
+transit_access_in = list()
+fw_management_access_list = list()
+
+
+def list_ifname_ip():
+
+	for v in fopen:
+		if "interface" in v:
+			v=v.split()
+			interface_list.append(v[1])
+		if "nameif" in v:
+			v=v.split()
+			if "no" in v[0]:
+				interface_list.append("null")
+			else:
+				interface_list.append(v[1])
+		return intdict
+print(list_ifname_ip())
