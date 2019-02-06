b86a5a40d1fa56c3a939e90ee9dbf4fe9fc7df5b
patelnancy12 <Nancy.Patel@dal.ca>
2019-02-06 18:17:54 +0000

    task done

--- model1.py
+++ model1.py
@@ -3,25 +3,19 @@
 fopen=open("running-config.cfg",'r')
 fopen=fopen.read()
 fopen=fopen.split("\n")
-final_list = list()
-interface_list = list()
-intdict=dict()
-access_list =  list()
-transit_access_in = list()
-fw_management_access_list = list()
+final_list = []
+interface_list = []
 
+intdict = dict()
 
 def list_ifname_ip():
 
-	for v in fopen:
-		if "interface" in v:
-			v=v.split()
-			interface_list.append(v[1])
-		if "nameif" in v:
-			v=v.split()
-			if "no" in v[0]:
-				interface_list.append("null")
-			else:
-				interface_list.append(v[1])
-		return intdict
-print(list_ifname_ip())
+  for v in fopen:
+    if "ip address" in v:
+      v=v.split()
+      interface_list.append(v)
+      print(interface_list)
+     return intdict
+   print(list_ifname_ip())
+
+list_ifname_ip()
